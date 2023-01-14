51525580c013be1341f09104b49835cac9fc9f1d
Dino <8dino2@gmail.com>
2023-01-14 00:44:33 -0500

    Added Patman and old aq2zone site

---
+++ archive/websites/aq2zone.actionmods.com/groupcp.php
@@ -0,0 +1,137 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html dir="ltr">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta http-equiv="Content-Style-Type" content="text/css">
+<link rel="top" href="./index.php?sid=a0b0dd5ec5c15f1e57e83100872773aa" title="Action Message Boards Forum Index" />
+<link rel="search" href="./search.php?sid=a0b0dd5ec5c15f1e57e83100872773aa" title="Search" />
+<link rel="help" href="./faq.php?sid=a0b0dd5ec5c15f1e57e83100872773aa" title="FAQ" />
+<link rel="author" href="./memberlist.php?sid=a0b0dd5ec5c15f1e57e83100872773aa" title="Memberlist" />
+<title>Action Message Boards // Group Control Panel</title>
+<link rel="stylesheet" href="templates/amb/amb.css" type="text/css">
+</head>
+
+<body link="#006699" vlink="#5493B4" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+
+<a name="top"></a>
+<table class="mainwindow" cellspacing="0" cellpadding="0">
+<tr>
+<td>
+
+<table width="100%" cellpadding="0" cellspacing="0">
+<tr>
+<td valign="top">
+
+
+<table width="100%" cellspacing="0" cellpadding="0">
+<tr>
+<td class="title"><table width="100%" cellpadding="0" cellspacing="0"><tr>
+<td valign="bottom"><div align="right"><img src="templates/amb/images/logo.gif"></img></div></td></tr></table></td>
+</tr>
+<tr>
+<td class="subtitle">
+<table width="100%" cellpadding="0" cellspacing="0">
+<tr>
+<td class="small">
+<div align="left">
+
+        <a href="index.php?sid=a0b0dd5ec5c15f1e57e83100872773aa">Action Message Boards Forum Index</a> ::
+        <a href="faq.php?sid=a0b0dd5ec5c15f1e57e83100872773aa">FAQ</a> ::
+        <a href="search.php?sid=a0b0dd5ec5c15f1e57e83100872773aa">Search</a> ::
+        <a href="memberlist.php?sid=a0b0dd5ec5c15f1e57e83100872773aa">Memberlist</a> ::
+        <a href="groupcp.php?sid=a0b0dd5ec5c15f1e57e83100872773aa">Usergroups</a>
+
+</div>
+</td>
+<td width="35%" class="small">
+<div align="right">
+
+
+                                <a href="profile.php?mode=register&amp;sid=a0b0dd5ec5c15f1e57e83100872773aa">Register</a> ::
+                                 <a href="login.php?sid=a0b0dd5ec5c15f1e57e83100872773aa">Log in</a> ::
+                <a href="profile.php?mode=editprofile&amp;sid=a0b0dd5ec5c15f1e57e83100872773aa">Profile</a> :: 
+		<a href="privmsg.php?folder=inbox&amp;sid=a0b0dd5ec5c15f1e57e83100872773aa">Log in to check your private messages</a>
+
+
+</div>
+</td>
+</tr>
+</table>
+
+</td>
+</tr>
+</table>
+
+<table width="100%" cellspacing="0" cellpadding="10">
+<tr>
+<td>
+
+ 
+<table width="100%" cellspacing="2" cellpadding="2" border="0" align="center">
+  <tr> 
+        <td align="left"><span class="nav"><a href="index.php?sid=a0b0dd5ec5c15f1e57e83100872773aa" class="nav">Action Message Boards Forum Index</a></span></td>
+  </tr>
+</table>
+
+<table width="100%" cellpadding="4" cellspacing="1" border="0" class="forumborder">
+      <tr> 
+        <th colspan="2" align="center" class="forumtitle" height="25">Join a Group</th>
+  </tr>
+  <tr> 
+        <td class="row1"><span class="gen">Non-member groups</span></td>
+        <td class="row2" align="right"> 
+          <table width="90%" cellspacing="0" cellpadding="0" border="0">
+                <tr><form method="get" action="groupcp.php?sid=a0b0dd5ec5c15f1e57e83100872773aa">
+                        <td width="40%"><span class="gensmall"><select name="g"><option value="120">Content Moderators</option><option value="122">General Moderators</option><option value="121">Mod Moderators</option><option value="179">Open Reaction</option><option value="351">Open Reaction Dev Team</option><option value="271">Photography Mods</option><option value="95">Super Moderators</option></select></span></td>
+                        <td align="center" width="30%"> 
+                          <input type="submit" value="View Information" class="liteoption" /><input type="hidden" name="sid" value="a0b0dd5ec5c15f1e57e83100872773aa" />                        </td>
+                </form></tr>
+          </table>
+        </td>
+  </tr>
+  </table>
+
+<table width="100%" cellspacing="2" border="0" align="center" cellpadding="2">
+  <tr> 
+        <td align="right" valign="top"><span class="gensmall">All times are GMT - 4 Hours</span></td>
+  </tr>
+</table>
+
+<br clear="all" />
+
+<table width="100%" cellspacing="2" border="0" align="center">
+  <tr> 
+        <td valign="top" align="right"><form method="get" name="jumpbox" action="viewforum.php?sid=a0b0dd5ec5c15f1e57e83100872773aa" onSubmit="if(document.jumpbox.f.value == -1){return false;}"><table cellspacing="0" cellpadding="0" border="0">
+        <tr> 
+                <td nowrap="nowrap"><span class="gensmall">Jump to:&nbsp;<select name="f" onchange="if(this.options[this.selectedIndex].value != -1){ forms['jumpbox'].submit() }"><option value="-1">Select a forum</option><option value="-1">&nbsp;</option><option value="-1">News</option><option value="-1">----------------</option><option value="10">News</option><option value="-1">&nbsp;</option><option value="-1">Action Mods</option><option value="-1">----------------</option><option value="3">Action UT</option><option value="4">Action Half Life</option><option value="5">Action Quake 2</option><option value="-1">&nbsp;</option><option value="-1">General</option><option value="-1">----------------</option><option value="2">Silly Hats Only</option><option value="6">Movie Reviews</option><option value="7">Product Reviews</option><option value="11">Game Reviews</option><option value="-1">&nbsp;</option><option value="-1">Content Creation</option><option value="-1">----------------</option><option value="13">Haven O' Frags</option><option value="15">AHLMD</option><option value="14">AQMD</option><option value="-1">&nbsp;</option><option value="-1">Photography</option><option value="-1">----------------</option><option value="17">Photo Contest</option><option value="18">General Photography Discussion</option></select><input type="hidden" name="sid" value="a0b0dd5ec5c15f1e57e83100872773aa" />&nbsp;<input type="submit" value="Go" class="liteoption" /></span></td>
+        </tr>
+</table></form>
+</td>
+  </tr>
+</table>
+<div align="center"><span class="copyright"><br /></span>
+
+<div class="small">amb v0.1  // Theme Created By: <a href="http://www.andrewcharron.tk">Andrew Charron</a> // Icons in Part By: <a href="http://www.apexmanifesto.com/">Travis Carden</a> // Modified By: Evan Prentice.</div>
+
+</td>
+</tr>
+</table>
+
+</td>
+</tr>
+<tr>
+<td valign="bottom" height="130">
+
+<table width="100%" cellpadding="0" cellspacing="0">
+<tr>
+<td class="subtitle"><div align="center">Powered by <a href="http://www.phpbb.com/" target="_phpbb" class="copyright">phpBB</a>  &copy; 2001, 2002 phpBB Group<br /></div></td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+</body>
+</html>
